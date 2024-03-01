ddc78d99a66b1657ec21b9b174996daa4997a8dc
MarcoPaoloni06 <marco.paoloni.studio@fitstic-edu.com>
2024-03-01 17:38:56 +0100

    InitialMarco

---
+++ lib/ip_check.dart
@@ -0,0 +1,21 @@
+// https://api.ipify.org
+import 'models.dart';
+import 'dart:convert';
+// Importa il modulo http da package:http/http.dart e lo rinomina come http.
+import "package:http/http.dart" as http;
+
+// Definisce una costante stringa contenente l'URL per l'API ipify.org.
+const stringUrl= "https://ipapi.co/json";
+
+// Definisce una funzione chiamata getIp che restituisce una stringa.
+Future<Ip> getIp() async {
+  // Converte la costante stringUrl in un oggetto Uri.
+  final url = Uri.parse(stringUrl);
+  
+  // Effettua una richiesta GET all'URL fornito.
+  final res=await http.get(url);
+
+  final Map<String, dynamic> data= json.decode(res.body);
+
+  return Ip(ip: data['ip'], region: data['region'],postal: data['postal'],city: data['city']);
+}
